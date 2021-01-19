a6a7a96f5ac316fd23a54d1bc7f2aeb64f4f917e
Haydenchung03 <haydenchung21@hotmail.com>
2021-01-19 13:49:30 -0500

    set up database file

---
+++ src/basic/Database.java
@@ -0,0 +1,17 @@
+package basic;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Database extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
