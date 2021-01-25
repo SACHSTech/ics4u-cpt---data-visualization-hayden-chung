f0a69120a8f21d4a4b5a807734b446cccd04d6dc
Haydenchung03 <haydenchung21@hotmail.com>
2021-01-24 20:53:51 -0500

    setting up list

--- src/basic/Database.java
+++ src/basic/Database.java
@@ -1,7 +1,10 @@
 package basic;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -9,6 +12,11 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Database extends Application {
+
+    public Parent createContent() {
+        final ObservableList<sports>
+    }
+
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/basic/sportslist.java
@@ -0,0 +1,23 @@
+package basic;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+public class sportslist {
+
+    private StringProperty gameNumber;
+    private StringProperty dateNumber;
+    private StringProperty mostScored;
+    private StringProperty gameLocation;
+    private StringProperty gameOpponent;
+    private StringProperty gameResult;
+    private StringProperty gameLength;
+    private StringProperty raptorScore;
+    private StringProperty opponentScore;
+    private StringProperty totalWins;
+    private StringProperty totalLoses;
+    private StringProperty gameStreak;
+
+
+}
