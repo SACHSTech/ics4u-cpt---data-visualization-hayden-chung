87e3044bb528143cf5489715765398ec6d6f9df9
Haydenchung03 <haydenchung21@hotmail.com>
2021-01-25 19:21:20 -0500

    creating objects for sports list

--- src/basic/sportslist.java
+++ src/basic/sportslist.java
@@ -19,5 +19,18 @@ public class sportslist {
     private StringProperty totalLoses;
     private StringProperty gameStreak;
 
-
+    public sportslist(String gameNumber, String dateNumber, String mostScored, String gameLocation, String gameOpponent, String gameResult, String gameLength, String raptorScore, String opponentScore, String totalWins, String totalLoses, String gameStreak) {
+        this.gameNumber = new SimpleStringProperty(gameNumber);
+        this.dateNumber = new SimpleStringProperty(dateNumber);
+        this.mostScored = new SimpleStringProperty(mostScored);
+        this.gameLocation = new SimpleStringProperty(gameLocation);
+        this.gameOpponent = new SimpleStringProperty(gameOpponent);
+        this.gameResult = new SimpleStringProperty(gameResult);
+        this.gameLength = new SimpleStringProperty(gameLength);
+        this.raptorScore = new SimpleStringProperty(raptorScore);
+        this.opponentScore = new SimpleStringProperty(opponentScore);
+        this.totalWins = new SimpleStringProperty(totalWins);
+        this.totalLoses = new SimpleStringProperty(totalLoses);
+        this.gameStreak = new SimpleStringProperty(gameStreak);
+    }
 }
