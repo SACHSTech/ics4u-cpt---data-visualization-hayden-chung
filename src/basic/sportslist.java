036904cb86dcea461efa7367f08b504a81adf8d0
Haydenchung03 <haydenchung21@hotmail.com>
2021-01-25 23:39:29 -0500

    returning objects

--- src/basic/sportslist.java
+++ src/basic/sportslist.java
@@ -2,35 +2,95 @@ package basic;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 
 
 public class sportslist {
 
-    private StringProperty gameNumber;
+    private IntegerProperty gameNumber;
     private StringProperty dateNumber;
-    private StringProperty mostScored;
+    private StringProperty gameTime;
+    private StringProperty bestPlayer;
     private StringProperty gameLocation;
     private StringProperty gameOpponent;
     private StringProperty gameResult;
     private StringProperty gameLength;
-    private StringProperty raptorScore;
-    private StringProperty opponentScore;
-    private StringProperty totalWins;
-    private StringProperty totalLoses;
+    private IntegerProperty raptorScore;
+    private IntegerProperty opponentScore;
+    private IntegerProperty totalWins;
+    private IntegerProperty totalLoses;
     private StringProperty gameStreak;
 
-    public sportslist(String gameNumber, String dateNumber, String mostScored, String gameLocation, String gameOpponent, String gameResult, String gameLength, String raptorScore, String opponentScore, String totalWins, String totalLoses, String gameStreak) {
-        this.gameNumber = new SimpleStringProperty(gameNumber);
+    public sportslist(int gameNumber, String dateNumber, String gameTime, String bestPlayer, String gameLocation, String gameOpponent, String gameResult, String gameLength, int raptorScore, int opponentScore, int totalWins, int totalLoses, String gameStreak) {
+        this.gameNumber = new SimpleIntegerProperty(gameNumber);
         this.dateNumber = new SimpleStringProperty(dateNumber);
-        this.mostScored = new SimpleStringProperty(mostScored);
+        this.gameTime = new SimpleStringProperty(gameTime);
+        this.bestPlayer = new SimpleStringProperty(bestPlayer);
         this.gameLocation = new SimpleStringProperty(gameLocation);
         this.gameOpponent = new SimpleStringProperty(gameOpponent);
         this.gameResult = new SimpleStringProperty(gameResult);
         this.gameLength = new SimpleStringProperty(gameLength);
-        this.raptorScore = new SimpleStringProperty(raptorScore);
-        this.opponentScore = new SimpleStringProperty(opponentScore);
-        this.totalWins = new SimpleStringProperty(totalWins);
-        this.totalLoses = new SimpleStringProperty(totalLoses);
+        this.raptorScore = new SimpleIntegerProperty(raptorScore);
+        this.opponentScore = new SimpleIntegerProperty(opponentScore);
+        this.totalWins = new SimpleIntegerProperty(totalWins);
+        this.totalLoses = new SimpleIntegerProperty(totalLoses);
         this.gameStreak = new SimpleStringProperty(gameStreak);
+    }
+
+    public IntegerProperty gameNumberProperty() {
+        return gameNumber;
+    }
+
+    public StringProperty dateNumberProperty() {
+        return dateNumber;
+    }
+
+    public StringProperty gameTimeProperty() {
+        return gameTime; 
+    }
+
+    public StringProperty bestPlayerProperty() {
+        return bestPlayer;
+    }
+
+    public StringProperty gameLocationProperty() {
+        return gameLocation;
+    }
+
+    public StringProperty gameOpponentProperty() {
+        return gameOpponent;
+    }
+
+    public StringProperty gameResultProperty() {
+        return gameResult;
+    }
+
+    public StringProperty gameLengthProperty() {
+        return gameLength;
+    }
+
+    public IntegerProperty raptorScoreProperty() {
+        return raptorScore;
+    }
+
+    public IntegerProperty opponentScoreProperty() {
+        return opponentScore;
+    }
+
+    public IntegerProperty totalWinsProperty() {
+        return totalWins;
+    }
+
+    public IntegerProperty totalLossesProperty() {
+        return totalLoses;
+    }
+
+    public StringProperty gameStreakProperty() {
+        return gameStreak;
     }
 }
