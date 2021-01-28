9e81cabb3b54e44bab2f5e08be9db3c27f39754a
Haydenchung03 <haydenchung21@hotmail.com>
2021-01-28 16:31:20 -0500

    fixed error

--- src/basic/MainMenu.java
+++ src/basic/MainMenu.java
@@ -69,56 +69,74 @@ public class MainMenu {
                     if(strChoice.equalsIgnoreCase("Database")){
                         System.out.println( sportsList = new SportsList(intGameNumber, strDateNumber, strGameTime, strGameLocation, strGameOpponent, strGameResult, strGameLength, intRaptorScore, intOpponentScore, intTotalWins, intTotalLosses, strGameStreak));
                         sportsArray[intCount] = sportsList;
-                        blnLoop = true;
                     }else {
                         sportsList = new SportsList(intGameNumber, strDateNumber, strGameTime, strGameLocation, strGameOpponent, strGameResult, strGameLength, intRaptorScore, intOpponentScore, intTotalWins, intTotalLosses, strGameStreak);
                         sportsArray[intCount] = sportsList;
                     }
                 }
             SportsData.close();
+            SportsData = new BufferedReader(new FileReader("src/basic/sports.csv"));
             if (strChoice.equalsIgnoreCase("Algorithms")) {
                 System.out.println("Type out which algorithm you would like to use");
                 System.out.println("Type in 1 if you want to search a certain game streak");
                 System.out.println("Type in 2 if you want to search playoff games only");
                 System.out.println("Type in 3 if you want to search regular season games only");
                 System.out.println("Type in 4 if you want to sort the Raptors score by lowest amount");
-                System.out.println("")
+                System.out.println("Type in 5 if you want to sort the opponents score by lowest amount");
+                System.out.println("Type in 6 if you want to sort the games by wins or losses");
+                System.out.println("Type in 7 if you want to sort the games by highest scored by a Raptor");
+                System.out.println("Type in 8 if you want to sort the games by highest scored by a opponent");
+                System.out.println("Type in 9 if you want to find games vs a certain opponent");
+                System.out.println("Type in 10 if you want to sort games by home or away");
+                System.out.println("Type in 11 if you want to see the average amount of points scored by the Raptors");
+                System.out.println("Type in 12 if you want to see the average amount of points scored on the Raptors");
+                System.out.println("Type in 13 if you want to see specific game");
                 intOption = Integer.parseInt(keyboard.readLine());
-                System.out.println("Type a game streak for wins or losses and the amount next to it (From: L 1 - L 2 and W 1 - W 8 ");
-                strGameStreak = keyboard.readLine();
-                System.out.println(DataSorting.searchWinStreak(sportsArray, strGameStreak));
-                // Print out only playoff or regular season games
-                System.out.println(DataSorting.searchRegular(sportsArray));
-                System.out.println(DataSorting.searchPlayoff(sportsArray));
-                
-                // Sort Lowest Points
-                System.out.println(DataSorting.sortLowestPoints(sportsArray));
-                System.out.println(DataSorting.sortLowestOpponentPoints(sportsArray));
-                // Sort by wins or losses
-                System.out.println("Type W or L to sort Raptors Wins and losses");
-                strWinsOrLoss = keyboard.readLine();
-                System.out.println(DataSorting.searchResults(sportsArray, strWinsOrLoss));
-                // Sorting points from raptor and opponents
-                System.out.println(DataSorting.sortHighestPoints(sportsArray));
-                System.out.println(DataSorting.sortHighestOpponentPoints(sportsArray));
-                // Opponents
-                System.out.println("Type in opponent you want to search");
-                strOpponentChoice = keyboard.readLine();
-                System.out.println(DataSorting.searchOpponent(sportsArray, strOpponentChoice));
-        
-                // Away or Home games
-                System.out.println("Type Away or Home");
-                strLocation = keyboard.readLine();
-                System.out.println(DataSorting.searchHome(sportsArray, strLocation));
-
-                // Average points scored for Raptors and Opponents
-                System.out.println(DataSorting.averagePoints(sportsArray));
-                System.out.println(DataSorting.averagePoints2(sportsArray));
-            
-                // Search specific game number
-                System.out.println("Type a specific game number to print it out from 1-106");
-                intGameNumber1 = Integer.parseInt(keyboard.readLine());
-                System.out.println(DataSorting.gameNumber(sportsArray, intGameNumber1));
+                if (intOption == 1){
+                    System.out.println("Type a game streak for wins or losses and the amount next to it (From: L 1 - L 2 and W 1 - W 8 ");
+                    strGameStreak = keyboard.readLine();
+                    System.out.println(DataSorting.searchWinStreak(sportsArray, strGameStreak));
+                }else if (intOption == 2) {
+                    // Print out only playoff or regular season games
+                    System.out.println(DataSorting.searchRegular(sportsArray));
+                }else if (intOption == 3) {    
+                    System.out.println(DataSorting.searchPlayoff(sportsArray));
+                } else if (intOption == 4) {
+                    // Sort Lowest Points
+                    System.out.println(DataSorting.sortLowestPoints(sportsArray));
+                } else if (intOption == 5){
+                    System.out.println(DataSorting.sortLowestOpponentPoints(sportsArray));
+                } else if (intOption == 6) {
+                    // Sort by wins or losses
+                    System.out.println("Type W or L to sort Raptors Wins and losses");
+                    strWinsOrLoss = keyboard.readLine();
+                    System.out.println(DataSorting.searchResults(sportsArray, strWinsOrLoss));
+                } else if (intOption == 7) {
+                    // Sorting points from raptor and opponents
+                    System.out.println(DataSorting.sortHighestPoints(sportsArray));
+                } else if (intOption == 8) {
+                    System.out.println(DataSorting.sortHighestOpponentPoints(sportsArray));
+                } else if (intOption == 9){
+                    // Opponents
+                    System.out.println("Type in opponent you want to search");
+                    strOpponentChoice = keyboard.readLine();
+                    System.out.println(DataSorting.searchOpponent(sportsArray, strOpponentChoice));
+                } else if (intOption == 10) {
+                    // Away or Home games
+                    System.out.println("Type Away or Home");
+                    strLocation = keyboard.readLine();
+                    System.out.println(DataSorting.searchHome(sportsArray, strLocation));
+                } else if (intOption == 11) {
+                    // Average points scored for Raptors and Opponents
+                    System.out.println(DataSorting.averagePoints(sportsArray));
+                } else if (intOption == 12) {
+                    System.out.println(DataSorting.averagePoints2(sportsArray));
+                } else if (intOption == 13) {
+                    // Search specific game number
+                    System.out.println("Type a specific game number to print it out from 1-106");
+                    intGameNumber1 = Integer.parseInt(keyboard.readLine());
+                    System.out.println(DataSorting.gameNumber(sportsArray, intGameNumber1));
+                }
             }
         }
     }
